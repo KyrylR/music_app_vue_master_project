42bf80a0e49078c0a54f77c1d71b28824c4cd4d7
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-07-14 16:37:33 +0300

    Add router files

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,72 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import store from '@/store';
+
+const HomeView = () => import('@/views/HomeView.vue');
+const ManageView = () => import(/* webpackChunkName: "groupedChunk" */'@/views/ManageView.vue');
+const SongView = () => import(/* webpackChunkName: "groupedChunk" */'@/views/SongView.vue');
+const AboutView = () => import('@/views/AboutView.vue');
 
 const routes = [
   {
-    path: '/',
     name: 'home',
+    path: '/',
     component: HomeView,
   },
   {
-    path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/about',
+    component: AboutView,
+  },
+  {
+    name: 'manage',
+    // alias: 'manage',
+    path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
+    component: ManageView,
+    beforeEnter: (_to, _from, next) => {
+      // console.log('Manage Route Guard!');
+      // console.log(to, from);
+
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: SongView,
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, _from, next) => {
+  // console.log('Global Guard!');
+  // console.log(to, from);
+
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.auth.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
